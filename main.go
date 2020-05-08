8a0233a9b2744b11f25df95a06bf618eb11d2123
Nahuel Cirocco <nahuel.cirocco@gmail.com>
2020-05-08 22:44:21 +0200

    Added merge bins and generate cu2

--- main.go
+++ main.go
@@ -6,28 +6,94 @@ import (
 	"os"
 	"path/filepath"
 
+	cuetocu2 "github.com/ncirocco/cue-to-cu2"
 	binmerge "github.com/ncirocco/psx-bin-merge"
 	psxserialnumber "github.com/ncirocco/psx-serial-number"
 )
 
 func main() {
-	cueFiles, err := getCueFiles()
+	originPath := "."
+	outputPath := "output"
+
+	if len(os.Args) > 1 {
+		originPath = os.Args[1]
+	}
+
+	if len(os.Args) > 2 {
+		outputPath = os.Args[2]
+	}
+
+	if _, err := os.Stat(originPath); os.IsNotExist(err) {
+		log.Fatal(err)
+	}
+
+	err := mergeBins(originPath, outputPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = generateCu2(outputPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func mergeBins(originPath string, outputPath string) error {
+	cueFiles, err := getFilesByExtension(originPath, "cue")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	for _, cue := range cueFiles {
-		err = binmerge.Merge(cue, "carpeta")
+		err := binmerge.Merge(cue, outputPath)
 		if err != nil {
-			log.Fatal(err)
+			return err
 		}
 	}
 
-	if true {
-		return
+	return nil
+}
+
+func generateCu2(outputPath string) error {
+	mergedCueFiles, err := getFilesByExtension(outputPath, "cue")
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	bins, err := checkDirectory()
+	for _, cue := range mergedCueFiles {
+		err := cuetocu2.Generate(cue, filepath.Dir(cue))
+		os.Remove(cue)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func getFilesByExtension(path string, extension string) ([]string, error) {
+	var files []string
+	err := filepath.Walk(path,
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+
+			if filepath.Ext(path) == "."+extension {
+				files = append(files, path)
+			}
+
+			return nil
+		})
+	if err != nil {
+		log.Println(err)
+	}
+
+	return files, err
+}
+
+func getSerial(outputPath string) {
+	bins, err := getFilesByExtension(outputPath, "bin")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -41,46 +107,4 @@ func main() {
 
 		fmt.Println(serial)
 	}
-}
-
-func getCueFiles() ([]string, error) {
-	var bins []string
-	err := filepath.Walk("./ISOs",
-		func(path string, info os.FileInfo, err error) error {
-			if err != nil {
-				return err
-			}
-
-			if filepath.Ext(path) == ".cue" {
-				bins = append(bins, path)
-			}
-
-			return nil
-		})
-	if err != nil {
-		log.Println(err)
-	}
-
-	return bins, err
-}
-
-func checkDirectory() ([]string, error) {
-	var bins []string
-	err := filepath.Walk("./ISOs",
-		func(path string, info os.FileInfo, err error) error {
-			if err != nil {
-				return err
-			}
-
-			if filepath.Ext(path) == ".bin" {
-				bins = append(bins, path)
-			}
-
-			return nil
-		})
-	if err != nil {
-		log.Println(err)
-	}
-
-	return bins, err
 }
